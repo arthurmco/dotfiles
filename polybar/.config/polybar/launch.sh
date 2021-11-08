4d04e3819c38596e6b2f6b44561bec371d6b4c22
Arthur Mendes <arthurmco@gmail.com>
2021-11-08 12:35:20 -0300

    Fix dotfiles so they can run on nixOS

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,3 +1,7 @@
-killall -q polybar
+#!/bin/sh
+
+# a more portable way to kill a process
+# killall polybar does not work on NixOS
+kill -9 $(ps ax | grep $(which polybar) | awk '{print $1}')
 
 polybar example
