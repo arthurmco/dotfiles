aa76c21a125ae9a5c48599570ff37be3c3f31dea
Arthur Mendes <arthurmco@gmail.com>
2022-04-24 06:55:34 -0300

    Add a thing so EMACS' tramp-mode works with fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
 set -gx EDITOR vim
 
 direnv hook fish | source
+
+if test "$TERM" = "dumb"
+  function fish_prompt
+    echo "\$ "
+  end
+
+  function fish_right_prompt; end
+  function fish_greeting; end
+  function fish_title; end
+end
